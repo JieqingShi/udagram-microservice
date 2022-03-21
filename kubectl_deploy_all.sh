a2e8af7315387bd8f58d661c3396d0494707422a
Jieqing Shi <Jieqing.Shi@bmwgroup.com>
2022-03-21 12:37:28 +0100

    Added yaml files

---
+++ kubectl_deploy_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+kubectl apply -f aws-secret.yaml
+kubectl apply -f env-secret.yaml
+kubectl apply -f env-configmap.yaml
+
+kubectl apply -f backend-feed-deployment.yaml
+kubectl apply -f backend-user-deployment.yaml
+kubectl apply -f frontend-deployment.yaml
+
+kubectl apply -f backend-feed-service.yaml
+kubectl apply -f backend-user-service.yaml
+kubectl apply -f frontend-service.yaml
+
+kubectl apply -f reverseproxy-deployment.yaml
+kubectl apply -f reverseproxy-service.yaml
